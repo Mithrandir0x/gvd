7387e299a59fe460058f85dc87902cd3650780a7
Mithrandir0x <mithrandir0x@gmail.com>
2015-02-20 09:11:18 +0100

    Added some comments in provaObjecte

--- P0/provaObjecte/main.cpp
+++ P0/provaObjecte/main.cpp
@@ -5,15 +5,25 @@ using namespace std;
 
 int main()
 {
+    cout << "Creant r0" << endl;
     Rectangle r0;
-    Rectangle r1 (2.0, 5.0);
-    Rectangle *r2;
+    cout << endl;
 
+    cout << "Creant r1" << endl;
+    Rectangle r1 (2.0, 5.0);
+    cout << endl;
+
+    cout << "Creant punter r2" << endl;
+    Rectangle *r2;
+    cout << endl;
+
+    cout << "Assignant nova instancia de Rectangle a punter r2" << endl;
     r2 = new Rectangle();
+    cout << endl;
 
     cout<< "Area de r0: " << r0.calculArea() << endl;
     cout<< "Area de r1: " << r1.calculArea() << endl;
-    cout<< "Area de r2: " << r2->calculArea()<<endl;
+    cout<< "Area de r2: " << r2->calculArea()<< endl;
 
     delete r2;
     return 0;
